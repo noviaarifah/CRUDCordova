3868f41c22a073e092761cfd9aa5dd943c3c0e9c
NOP <74388078+noviaarifah@users.noreply.github.com>
2021-03-25 00:27:21 +0700

    Initial commit

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id16353636_admin","(B/^b#9V-#{BUEcW","id16353636_akademik") or die ("could not connect database");
+?>
